fc7afb8105f2a073dbea5219016e23ffab6753d1
ghaleon7 <76746117+ghaleon7@users.noreply.github.com>
2021-11-27 14:28:27 +0000

    Update __init__.py
    A Chakravala vai passar a ter um cálculo recursivo, por isso, adicionei um módulo com essa recursão no init e o módulo com o cálculo das funções importantes.

--- Chakravala/__init__.py
+++ Chakravala/__init__.py
@@ -1,2 +1,4 @@
 from .chakravala import chakravala, chakravala_terno
 from .sqrt_approx import sqrt_approx, sqrt_app
+from ._utils import m_method, compose
+from .chakravala_recursion import chakravala_rec
